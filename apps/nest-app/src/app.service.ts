a08748cbbd9dc39f31302a3c95c5e372dfc950fe
hardfist <1562502418@qq.com>
2021-05-02 22:58:57 +0800

    feat: add nestjs

--- apps/nest-app/src/app.service.ts
+++ apps/nest-app/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { answer } from '@hardfist/utils';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + answer;
   }
 }
