511e7d7a96e1568f7c94617e5a2fb2ec580c26b7
hardfist <1562502418@qq.com>
2021-05-29 21:09:53 +0800

    feat: add recoil

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,45 +1,39 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import React, { useState } from 'react';
+import { atom, selector, useRecoilState, useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import './App.css';
+const fontSizeState = atom({
+  key: 'fontSizeState',
+  default: 14
+});
+const fontSizeLabelState = selector({
+  key: 'fontSizeLabelState',
+  get: ({ get }) => {
+    const fontSize = get(fontSizeState);
+    const unit = 'px';
+    return `${fontSize}${unit}`;
+  }
+});
+const todoListState = atom({
+  key: 'todoListState',
+  default: []
+});
 function App() {
-  const [count, setCount] = useState(0)
+  const [fontSize, setFontSize] = useRecoilState(fontSizeState);
+  const fontSizeLabel = useRecoilValue(fontSizeLabelState);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <div>count:{fontSize}</div>
+      <button
+        onClick={() => {
+          setFontSize((x) => x + 1);
+        }}
+      >
+        add
+      </button>
+      <div>current fontSize: {fontSizeLabel}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
