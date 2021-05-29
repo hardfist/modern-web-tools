b752d8e2c97af866b7af1bc8974f67a106b3d994
hardfist <1562502418@qq.com>
2021-05-29 21:09:53 +0800

    feat: add recoil

--- apps/react-app/src/main.tsx
+++ apps/react-app/src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RecoilRoot>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RecoilRoot>,
   document.getElementById('root')
-)
+);
