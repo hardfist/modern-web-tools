0ea690a10f23bda2b5da6da6faa5b35b2d41ce4d
hardfist <1562502418@qq.com>
2021-06-10 23:08:20 +0800

    feat: add babel support

---
+++ examples/sourcemap/src/babel.mjs
@@ -0,0 +1,22 @@
+import babel from '@babel/core';
+import fs from 'fs';
+const result = babel.transform('a === b;', {
+  sourceMaps: true,
+  filename: 'transform.js',
+  plugins: [
+    {
+      name: 'my-plugin',
+      pre: () => {
+        console.log('xx');
+      },
+      visitor: {
+        BinaryExpression(path, t) {
+          let tmp = path.node.left;
+          path.node.left = path.node.right;
+          path.node.right = tmp;
+        }
+      }
+    }
+  ]
+});
+console.log(result.code, result.map);
