bed376f708b313c9a5d991701ad36d1072306661
hardfist <1562502418@qq.com>
2021-08-22 16:32:57 +0800

    feat: ios setup

---
+++ ios/neo/Shared/neoApp.swift
@@ -0,0 +1,17 @@
+//
+//  neoApp.swift
+//  Shared
+//
+//  Created by yj on 2021/8/22.
+//
+
+import SwiftUI
+
+@main
+struct neoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
