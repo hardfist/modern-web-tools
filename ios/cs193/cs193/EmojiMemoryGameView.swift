4efb01beb0c89c2ee8f4ea91a00ea27e1536ed3d
hardfist <1562502418@qq.com>
2021-08-28 19:16:20 +0800

    feat: refactor swift code

--- ios/cs193/cs193/EmojiMemoryGameView.swift
+++ ios/cs193/cs193/EmojiMemoryGameView.swift
@@ -10,15 +10,27 @@ import SwiftUI
 struct CardView: View {
     var card: MemoryGame<String>.Card;
     var body: some View {
-        if(card.isFaceUp){
-            ZStack{
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10).stroke(lineWidth:3)
-                Text(card.content).font(.largeTitle)
-            }
-        }else {
-            RoundedRectangle(cornerRadius: 10.0).fill();
+        GeometryReader{ geometry in
+            self.body(for:geometry.size)
         }
+    }
+    func body(for size: CGSize) -> some View{
+        ZStack{
+        if(card.isFaceUp){
+        
+                RoundedRectangle(cornerRadius:cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth:edgeLineWidth)
+                Text(card.content)
+        }else {
+            RoundedRectangle(cornerRadius: cornerRadius).fill();
+        }
+        }.font(Font.system(size: self.fontSize(for: size)))
+    }
+    let cornerRadius: CGFloat = 10.0;
+    let edgeLineWidth: CGFloat = 3;
+    let fontScaleFactor: CGFloat = 0.75;
+    func fontSize(for size: CGSize) -> CGFloat{
+        return min(size.width, size.height) * self.fontScaleFactor;
     }
 }
 struct EmojiMemoryGameView: View {
